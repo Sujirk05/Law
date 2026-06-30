4229a6d28954d2d8ef8dcbde5db35aeb138057e7
Sujirk05 <sujithark.art@gmail.com>
2026-06-30 17:22:30 +0530

    changes made in about

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -1,8 +1,8 @@
 import { motion } from "framer-motion";
 function About() {
   return (
-    <section id="About"className="w-full py-20 px-10 bg-gray-300 scroll-mt-24 overflow-hidden" >
-      <div className="max-w-7xl mx-auto flex items-center  gap-16">
+    <section id="About"className="w-full min-h-screen py-28 px-12 bg-gray-300 scroll-mt-24 overflow-hidden" >
+      <div className="max-w-7xl mx-auto flex items-center  gap-40">
 
      
         {/* <div className="w-1/2 flex justify-center"> */}
@@ -13,7 +13,7 @@ function About() {
   className="w-1/2"
   initial={{ opacity: 0, x: -100}}
   whileInView={{ opacity: 1, x: 0 }}
-  transition={{ duration: 0.8 }}
+  transition={{ duration: 2 }}
   viewport={{ once: false, amount: 0.3 }}
 >
         <div className="">
@@ -41,21 +41,21 @@ function About() {
   className="w-1/2"
   initial={{ opacity: 0, x: 100 }}
   whileInView={{ opacity: 1, x: 0 }}
-  transition={{ duration: 0.8 }}
+  transition={{ duration: 2 }}
   viewport={{ once: false, amount: 0.3 }}
-><div className="relative w-96 h-80">
-           <div className=" absolute top-0 left-1/2 -translate-x-1/2 w-40 h-40  rounded-full bg-yellow-50 border-2 border-yellow-500 shadow-xl flex flex-col items-center justify-center">
+><div className="relative w-130 h-107">
+           <div className=" absolute top-0 left-1/2 -translate-x-1/2 w-52 h-52 rounded-full bg-yellow-50 border-2 border-yellow-500 shadow-xl flex flex-col items-center justify-center">
   <h3 className="text-4xl font-bold text-yellow-500">15+</h3>
   <p className="text-center text-sm px-3">
   Years of Experience
 </p>
 </div>
 
-                   <div className=" absolute bottom-0 left-0  w-40 h-40 rounded-full bg-yellow-50 border-2 border-yellow-500 shadow-xl flex flex-col items-center justify-center">
+                   <div className=" absolute bottom-0 left-0 w-52 h-52 rounded-full bg-yellow-50 border-2 border-yellow-500 shadow-xl flex flex-col items-center justify-center">
   <h3 className="text-4xl font-bold text-yellow-500">500+</h3>
   <p className="text-center text-sm px-3">cases handled</p>
 </div>
-          <div className=" absolute bottom-0 right-0 w-40 h-40 rounded-full bg-yellow-50 border-2 border-yellow-500 shadow-xl flex flex-col items-center justify-center">
+          <div className=" absolute bottom-0 right-0 w-52 h-52 rounded-full bg-yellow-50 border-2 border-yellow-500 shadow-xl flex flex-col items-center justify-center">
   <h3 className="text-4xl font-bold text-yellow-500">98%</h3>
   <p className="text-center text-sm px-3">Success Rate</p>
 </div>
